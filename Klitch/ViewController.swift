0f3571ea6cf1de2286fb56aa679a4cfb546cc514
OUT-Samoylov1-OA <Samoylov.O.A@sberbank.ru>
2021-05-22 17:41:16 +1000

    Initial Commit

---
+++ Klitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Klitch
+//
+//  Created by Oleg Samoylov on 22.05.2021.
+//  Copyright © 2021 Sberbank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
